c7d9eae4d9c942950fb7fcf5000691053cb82cb0
duy <duydq2k1@gmail.com>
2023-10-17 02:57:43 +0700

    fix env

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,14 +3,16 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     UserModule,
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '600000s' },
     }),
   ],
   controllers: [AuthController],
